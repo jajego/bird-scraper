1536ce843b52fabd1cbc2b64bdab51b849f9c2cf
Jamie <yames.git@gmail.com>
2022-11-06 11:51:00 -0500

    First commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,49 @@
+import os
+
+from scraper import getImageUrls
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5001)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/birds', methods=("GET", "POST"))
+    def getUrls():
+        print('Hello from / route')
+        if request.method == "GET":
+            birds = [];
+            print(request.args)
+            for arg in request.args:
+                birds.append(request.args.get(arg))        
+            urls = getImageUrls(birds)
+            return urls
+
+    return app
---
+++ scraper.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import requests;
+import time;
+
+def getImages(birds):
+    images = []
+    for bird in birds:
+        page = requests.get(f"https://en.wikipedia.org/wiki/{bird}")
+        soup = BeautifulSoup(page.content, 'html.parser')
+        images.append(soup.find_all("img"))
+    
+    return images;
+
+# write it so if it encounters a repeat it can just reference it
+# if url is invalid, find the next
+
+def getImageUrls(birds):
+    start = time.time()
+    
+    urls = []
+    images = getImages(birds)
+
+    for index in range(len(images)):
+        image = str(images[index][0])
+        print(image)
+        src_index = image.find("src")
+        print(src_index)
+        jpg_index = image.find("jpg/")
+        if jpg_index == -1:
+            jpg_index = image.find("JPG/")
+        print(jpg_index)
+        final_url = "https://" + image[src_index+7:jpg_index+3].replace("thumb/", "")
+        print(f"Final url in first loop {final_url}")
+        # Accounts for images on the page that come before the main bird image, such as "Good Page"
+        if final_url == "https://":
+            image = str(images[index][1])
+            print(image)
+            src_index = image.find("src")
+            jpg_index = image.find("jpg/")
+            final_url = "https://" + image[src_index+7:jpg_index+3].replace("thumb/", "")
+        
+        urls.append(final_url)
+
+    print(urls)
+    end = time.time()
+    print(f"Retrieved {len(birds)} images in {end-start} seconds")
+    return urls
